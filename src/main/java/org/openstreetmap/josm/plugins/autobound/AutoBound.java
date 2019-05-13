8753b1ea28f1d88b01a0b45878067d2a012a99eb
Vishal R <vishal27.rs@outlook.com>
2019-05-13 15:23:13 +0530

    Update Plugin Class

--- src/main/java/org/openstreetmap/josm/plugins/autobound/AutoBound.java
+++ src/main/java/org/openstreetmap/josm/plugins/autobound/AutoBound.java
@@ -1,3 +1,8 @@
-public class AutoBound{
-    public static void main(String[] args){ }
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class AutoBound extends Plugin {
+    public AutoBound(PluginInformation info) {
+        super(info);
+    }
 }
