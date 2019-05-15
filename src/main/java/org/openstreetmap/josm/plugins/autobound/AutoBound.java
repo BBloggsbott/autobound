e4e58cb50d92251d706048014880a45f4afb6148
Vishal R <vishal27.rs@outlook.com>
2019-05-15 11:22:09 +0530

    Add server_url property

--- src/main/java/org/openstreetmap/josm/plugins/autobound/AutoBound.java
+++ src/main/java/org/openstreetmap/josm/plugins/autobound/AutoBound.java
@@ -1,7 +1,11 @@
+package org.openstreetmap.josm.plugins.autobound;
+
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
+import org.openstreetmap.josm.data.preferences.StringProperty;
 
 public class AutoBound extends Plugin {
+    static StringProperty server_url = new StringProperty("server_url", "https://localhost:5000");
     public AutoBound(PluginInformation info) {
         super(info);
     }
