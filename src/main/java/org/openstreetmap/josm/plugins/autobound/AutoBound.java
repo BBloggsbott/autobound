4cd9c863842d69634c98e9b31ef195f9e69c33ab
Vishal R <vishal27.rs@outlook.com>
2019-05-24 22:01:01 +0530

    Change StringProperty key for server url

--- src/main/java/org/openstreetmap/josm/plugins/autobound/AutoBound.java
+++ src/main/java/org/openstreetmap/josm/plugins/autobound/AutoBound.java
@@ -5,7 +5,7 @@ import org.openstreetmap.josm.plugins.PluginInformation;
 import org.openstreetmap.josm.data.preferences.StringProperty;
 
 public class AutoBound extends Plugin {
-    static StringProperty server_url = new StringProperty("server_url", "https://localhost:5000");
+    static StringProperty server_url = new StringProperty("autoboundServerUrl", "https://localhost:5000");
     public AutoBound(PluginInformation info) {
         super(info);
     }
