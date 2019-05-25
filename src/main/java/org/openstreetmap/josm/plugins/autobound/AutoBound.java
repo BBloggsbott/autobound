c9dd0b058bdd1d8e99ba691d5700f392a1a03388
Vishal R <vishal27.rs@outlook.com>
2019-05-25 17:12:58 +0530

    Create Action Class and Add to Menu

--- src/main/java/org/openstreetmap/josm/plugins/autobound/AutoBound.java
+++ src/main/java/org/openstreetmap/josm/plugins/autobound/AutoBound.java
@@ -1,5 +1,7 @@
 package org.openstreetmap.josm.plugins.autobound;
 
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.MainMenu;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 import org.openstreetmap.josm.data.preferences.StringProperty;
@@ -8,5 +10,7 @@ public class AutoBound extends Plugin {
     static StringProperty server_url = new StringProperty("autoboundServerUrl", "https://localhost:5000");
     public AutoBound(PluginInformation info) {
         super(info);
+        MainMenu mainMenu = MainApplication.getMenu();
+        MainMenu.add(mainMenu.dataMenu, new AutoBoundAction(MainApplication.getMap(), server_url.get()));
     }
 }
---
+++ src/main/java/org/openstreetmap/josm/plugins/autobound/AutoBoundAction.java
@@ -0,0 +1,68 @@
+package org.openstreetmap.josm.plugins.autobound;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+import org.openstreetmap.josm.actions.mapmode.MapMode;
+import org.openstreetmap.josm.data.preferences.StringProperty;
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.MapFrame;
+import org.openstreetmap.josm.gui.MapView;
+import org.openstreetmap.josm.gui.SelectionManager;
+import org.openstreetmap.josm.gui.SelectionManager.SelectionEnded;
+import org.openstreetmap.josm.tools.ImageProvider;
+import org.openstreetmap.josm.tools.Shortcut;
+
+/**
+ * Enable AutoBound functionality within MapFrame.
+ *
+ * @author bbloggsbott
+ */
+
+public class AutoBoundAction extends MapMode implements SelectionEnded {
+
+    /**
+     * Manager that manages the selection rectangle with the aspect ration of the MapView
+     */
+    private final SelectionManager selectionManager;
+    private final String serverUrl;
+
+    /**
+     * Construct an AutoBoundAction without a Label
+     * @param mapFrame The MapFrame, whose autobound mode should be enabled.
+     */
+    //TODO : Modify Icon names
+    public AutoBoundAction(MapFrame mapFrame, String serverUrl){
+        super(tr("AutoBound Action"), "zoom",
+                tr("Generate nodes using AutoBound"), Shortcut.registerShortcut("mapmode:autobound", tr("Mode: {0}", tr("AutoBound mode")),KeyEvent.CHAR_UNDEFINED,Shortcut.NONE),
+                ImageProvider.getCursor("normal","zoom"));
+        this.selectionManager = new SelectionManager(this, true, mapFrame.mapView);
+        this.serverUrl = serverUrl;
+    }
+
+    //TODO : Modify selectionEnded method by getting MapView inside selection, encoding it and passing it to the server
+    @Override
+    public void selectionEnded(Rectangle r, MouseEvent e) {
+        System.out.println("[INFO] x1:"+r.x+" y1:"+r.y+" x2:"+(r.x+r.width)+" y2:"+(r.y+r.height));
+    }
+
+    @Override
+    public void enterMode() {
+        super.enterMode();
+        selectionManager.register(MainApplication.getMap().mapView, false);
+    }
+
+    @Override
+    public void exitMode() {
+        super.exitMode();
+        selectionManager.unregister(MainApplication.getMap().mapView);
+    }
+
+    @Override
+    public String getModeHelpText() {
+        return tr("Left Click and drag to select area for to detect buildings and generate boundaries");
+    }
+}
