cd1a9dbb05e4e5c60663d48632552ca0d7fe9238
Vishal R <vishal27.rs@outlook.com>
2019-05-31 15:10:41 +0530

    Add DataUtils xmlToDataSet test

--- src/test/java/org/openstreetmap/plugins/autobound/DataUtilsTests.java
+++ src/test/java/org/openstreetmap/plugins/autobound/DataUtilsTests.java
@@ -2,6 +2,9 @@ package org.openstreetmap.plugins.autobound;
 
 import org.json.JSONObject;
 import org.junit.Test;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.io.IllegalDataException;
 import org.openstreetmap.josm.plugins.autobound.DataUtils;
 
 import javax.imageio.ImageIO;
@@ -9,6 +12,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -52,5 +56,20 @@ public class DataUtilsTests {
         assertEquals(data.getString("image"), testJson.getString("image"));
     }
 
+    @Test
+    public void xmlToDataSetTest(){
+        DataSet dataset=null;
+        try{
+            dataset = DataUtils.xmlToDataSet(DataUtils.responseToInputStream("<?xml version='1.0' encoding='UTF-8'?><osm version='0.6' generator='AutoBoundTest'><bounds minlat='51.5076478723889' minlon='-0.127989783553507' maxlat='51.5077445145483' maxlon='-0.127774884645096' origin='AutoBoundTest' /><node id='26821100' timestamp='2009-02-16T21:34:57+00:00' visible='true' lat='51.5077286' lon='-0.1279688' version='1'/><osm>"));
+        } catch(IllegalDataException ide){
+            ide.printStackTrace();
+            fail("XML error");
+        }
+        Collection<Node> nodes = dataset.getNodes();
+        for (Node node: nodes){
+            assertEquals(51.5077286, node.getCoor().lat(),0.0);
+            assertEquals(-0.1279688, node.getCoor().lon(),0.0);
+        }
+    }
 
 }
