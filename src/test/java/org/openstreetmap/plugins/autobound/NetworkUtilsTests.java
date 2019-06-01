12d02960b95888d2f68e4aa3ee7b8222e404d32c
Vishal R <vishal27.rs@outlook.com>
2019-06-01 11:07:05 +0530

    Update NetworkUtilsTest

--- src/test/java/org/openstreetmap/plugins/autobound/NetworkUtilsTests.java
+++ src/test/java/org/openstreetmap/plugins/autobound/NetworkUtilsTests.java
@@ -7,21 +7,30 @@ import org.openstreetmap.josm.plugins.autobound.NetworkUtils;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
+/**
+ * Class to test NetworkUtils. AutoBound server must be running for these test to pass.
+ */
 public class NetworkUtilsTests {
 
+    /**
+     * Test the method to generate nodes.
+     */
     @Test
     public void generateNodesTest(){
         NetworkUtils networkUtils = null;
         JSONObject jsonObject = new JSONObject();
         try{
-            networkUtils = new NetworkUtils("http://localhost:5000/generateNodesTest/");
             jsonObject.put("verification","generateNodesTest");
+            networkUtils = new NetworkUtils("http://localhost:5000/generateNodesTest");
             String response = networkUtils.generateNodes(jsonObject);
             System.out.println(response);
             assertEquals("message exchange works", response);
+            jsonObject = new JSONObject();
+            jsonObject.put("verification","invalid");
+            response = networkUtils.generateNodes(jsonObject);
+            assertNotEquals("message exchange error", response);
         } catch(MalformedURLException mue){
             mue.printStackTrace();
             fail("Malformed URL");
