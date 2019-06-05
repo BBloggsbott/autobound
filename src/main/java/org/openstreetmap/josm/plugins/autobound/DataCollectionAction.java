1b3a5e39170156adcaa44d3eddae9942da9f97cf
Vishal R <vishal27.rs@outlook.com>
2019-06-05 12:01:17 +0530

    Add Action class to collect data

---
+++ src/main/java/org/openstreetmap/josm/plugins/autobound/DataCollectionAction.java
@@ -0,0 +1,78 @@
+package org.openstreetmap.josm.plugins.autobound;
+
+import org.json.JSONObject;
+import org.openstreetmap.josm.data.ProjectionBounds;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.layer.OsmDataLayer;
+import org.openstreetmap.josm.tools.Logging;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+/**
+ * Action class to collect imagery data
+ */
+public class DataCollectionAction implements ActionListener {
+
+    private NetworkUtils networkUtils = null;
+
+    /**
+     * Create a new DataCollectionAction object
+     * @param dataCollectionServerUrl URL to the server that saves data
+     */
+    public DataCollectionAction(String dataCollectionServerUrl){
+        try{
+            networkUtils = new NetworkUtils(dataCollectionServerUrl);
+        } catch (MalformedURLException mue){
+            Logging.error("Malformed Data Collection Server Url");
+        }
+    }
+
+    //TODO : Add ProgressMonitor
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(networkUtils != null){
+            OsmDataLayer dataLayer = MainApplication.getLayerManager().getActiveDataLayer();
+            DataSet dataset = dataLayer.getDataSet();
+            ArrayList<Way> buildings = DataUtils.getBuildingsFromDataSet(dataset);
+            BufferedImage image;
+            ProjectionBounds bounds;
+            JSONObject data;
+            String response;
+            int successfulSaves = 0, failedSaves = 0;
+
+            try{
+                for (Way building : buildings){
+                    bounds = MapUtils.getProjectionBoundsForImage(MapUtils.getBoundsForWay(building));
+                    image = MapUtils.getSatelliteImage(bounds);
+                    data = DataUtils.createJSON(image, bounds, building, MapUtils.getDist100Pixel());
+                    response = networkUtils.sendToServer(data);
+                    if(response.equalsIgnoreCase("success")){
+                        successfulSaves +=1;
+                    } else {
+                        failedSaves += 1;
+                    }
+                }
+            } catch (IOException ioe){
+                Logging.error("Error While communicating with server");
+            }
+            JOptionPane.showMessageDialog(
+                    MainApplication.getMainFrame(),
+                    tr("Successfully saved "+successfulSaves+" images out of "+(successfulSaves+failedSaves)+" images."),
+                    tr("Save Complete"),
+                    JOptionPane.INFORMATION_MESSAGE
+            );
+        } else {
+            Logging.error("Network not configured.");
+        }
+    }
+}
