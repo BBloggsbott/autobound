0a8f1f5c6f8284b8d2b4bc87dcace4ec2fb7894d
Vishal R <vishal27.rs@outlook.com>
2019-06-06 15:38:22 +0530

    Modify NetworkUtils to send data through POST

--- src/main/java/org/openstreetmap/josm/plugins/autobound/NetworkUtils.java
+++ src/main/java/org/openstreetmap/josm/plugins/autobound/NetworkUtils.java
@@ -1,12 +1,19 @@
 package org.openstreetmap.josm.plugins.autobound;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 import org.openstreetmap.josm.tools.Logging;
 
 import java.io.*;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.Scanner;
 
 /**
  * Provide Networking utilities for the plugin.
@@ -28,65 +35,33 @@ public class NetworkUtils {
         this.serverUrl = serverUrl;
     }
 
-    public String addDataToUrl(JSONObject data, String serverUrl){
-        String newServerUrl = serverUrl;
-        if(!serverUrl.substring(serverUrl.length()-1).equalsIgnoreCase("/")){
-            newServerUrl = serverUrl + "?data=" + data.toString();
-        }
-        return newServerUrl;
-    }
-
     /**
      * Generates the nodes by sending data to the server and returning the response from the server.
-     * @param data JSONObject containing the data to be sent to the server.
+     * @param jsonData JSONObject containing the data to be sent to the server.
      * @return The response form the server.
      * @throws IOException Thrown when an IOException is encountered.
      */
-    public String sendToServer(JSONObject data) throws IOException {
-        int httpResult;
-        URL url = new URL(addDataToUrl(data, serverUrl));
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        connection.setDoInput(true);
-        connection.setDoOutput(true);
-        connection.setRequestMethod("GET");
-        connection.setUseCaches(false);
+    public String sendToServer(JSONObject jsonData) throws IOException {
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost(serverUrl);
+        String responseString = null;
 
-        Logging.info("Sending data to AutoBound server at "+url);
-        OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-        out.write(data.toString());
-        out.close();
-        httpResult = connection.getResponseCode();
-        Logging.info("Response Code from AutoBound server: "+httpResult);
-        return readResponse(httpResult, connection);
-    }
+        NameValuePair data = new BasicNameValuePair("data", jsonData.toString());
+        httppost.setEntity(new StringEntity(jsonData.toString()));
 
-    /**
-     * Reads and returns the response from the server.
-     * @param httpResult The response code from the server.
-     * @param connection The HttpURLConnection object with the connection established to the server
-     * @return The response from the server.
-     */
-    public String readResponse(int httpResult, HttpURLConnection connection){
-        String response = null, line = null;
-        StringBuilder sb;
-        try{
-            if(httpResult==HttpURLConnection.HTTP_OK){
-                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));
-                sb = new StringBuilder();
-                while((line=br.readLine())!=null){
-                    sb.append(line);
-                }
-                br.close();
-                response = sb.toString();
-            }
-            else {
-                Logging.error("Error on posting "+connection.getResponseMessage());
+        Logging.info("Sending data to server at "+serverUrl);
+        HttpResponse response = httpclient.execute(httppost);
+        HttpEntity entity = response.getEntity();
+        Logging.info("Response Received");
+
+        if(entity != null){
+            InputStream instream = entity.getContent();
+            Scanner s = new Scanner(instream).useDelimiter("\n");
+            responseString="";
+            while(s.hasNext()){
+                responseString += s.next();
             }
         }
-        catch(IOException ioe){
-            Logging.error("Error in Response" + ioe.getMessage());
-        }
-        return response;
+        return responseString;
     }
-
 }
